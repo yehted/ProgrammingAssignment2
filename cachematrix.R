f90d1cbb9747f4d86aa0b8320d9872d8ab520588
yehted <yeh.ted@gmail.com>
2014-04-21 14:56:40 -0500

    Created files
    Files work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
+## Creates a data structure for calculating and caching
+## matrix inverses
 
-## Write a short comment describing this function
+## Creates a data structure for caching matrix inverses
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Solves for matrix inverse. Pulls from cache if existant
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	i <- x$getinv()
+	if (!is.null(i)) {
+		message("getting cached data")
+		return (i)
+	}
+	data <- x$get()
+	i <- solve(data, ...)
+	x$setinv(i)
+	i
 }
